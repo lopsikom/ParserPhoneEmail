1bdc7f6a11bb54d33a9018f3e2606f4ad3dd18f9
lopsik <lopsikom@yandex.ru>
2024-12-03 12:22:15 +0300

    Добавьте файлы проекта.

---
+++ ParserPhoneEmail/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParserPhoneEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
